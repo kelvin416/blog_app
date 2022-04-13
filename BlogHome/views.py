7ccd92a2205a4bbcbb327bfef185db7f41b35c68
kelvin416 <kelvinrotich416@gmail.com>
2022-04-13 22:01:31 +0300

    Created app templates

--- BlogHome/views.py
+++ BlogHome/views.py
@@ -1,9 +1,28 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+
+posts = [
+    {
+        "author": "KelvinR",
+        "title": "Blog post 1",
+        "content": "First post content",
+        "date_posted": "April 13, 2022"
+    },
+    {
+        "author": "Jane Doe",
+        "title": "Blog post 2",
+        "content": "Second post content",
+        "date_posted": "April 23, 2022"
+    },
+]
+
+
 def home(request):
-    return HttpResponse("<h1>Blog Home<h1>")
+    context = {
+        "posts": posts
+    }
+    return render(request, "BlogHome/home.html", context)
 
 
 def about(request):
-    return HttpResponse("<h1>Blog About<h1>")
+    return render(request, "BlogHome/about.html", {'title': 'About'})
