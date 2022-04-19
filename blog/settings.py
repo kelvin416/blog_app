1e77780b87096e8ca6d3c58abc10ccf563fe2469
kelvin416 <kelvinrotich416@gmail.com>
2022-04-19 14:37:55 +0300

    created login, logout and profile templates

--- blog/settings.py
+++ blog/settings.py
@@ -125,6 +125,10 @@ STATIC_URL = 'static/'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+# login url
+LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_URL = 'login'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
