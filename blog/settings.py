9bf3ea6c922809ddc6d6a3b341dfcc6de2507f37
kelvin416 <kelvinrotich416@gmail.com>
2022-04-19 09:50:17 +0300

    creating user registration page with email

--- blog/settings.py
+++ blog/settings.py
@@ -37,9 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    # Installed apps
     'BlogHome.apps.BloghomeConfig',
     'users.apps.UsersConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# adding bootstrap4
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
